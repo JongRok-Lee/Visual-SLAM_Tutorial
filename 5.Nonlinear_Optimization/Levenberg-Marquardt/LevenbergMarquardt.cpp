106609d8d4ad9d0eb8c6a6e5b0303874a41b7af7
JongRok-Lee <lrrghdrh@naver.com>
2022-12-03 00:27:12 +0900

    Nonlinear Optimization Update

--- 5.Nonlinear_Optimization/Levenberg-Marquardt/LevenbergMarquardt.cpp
+++ 5.Nonlinear_Optimization/Levenberg-Marquardt/LevenbergMarquardt.cpp
@@ -23,30 +23,31 @@ int main(int argc, char** argv) {
     // Start Gauss-Newton iterations
     int iterNumber = 200;
     double cost = 0, lastCost = 0;
+    double lambda = 1e-4;                               // Lambda for LM
     std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
     for (size_t iter = 0; iter < iterNumber; ++iter) {
-        Eigen::Matrix3d H = Eigen::Matrix3d::Zero();        // Hessian = J^T * W^{-1} * J
-        Eigen::Vector3d b = Eigen::Vector3d::Zero();        // Bias
         cost = 0;
-
+        Eigen::Matrix3d H = Eigen::Matrix3d::Zero();        // Hessian = J * J^T
+        Eigen::Matrix3d diag_H = Eigen::Matrix3d::Zero();   // diag(Hessian)
+        Eigen::Vector3d b = Eigen::Vector3d::Zero();        // Bias
+        Eigen::Vector3d J;
         for (size_t i = 0; i < N; ++i) {
             double xi = x_data[i], yi = y_data[i];          // The i-th data
             double y_hat = std::exp(ae*xi*xi + be*xi + ce); // Estimated y
             double error = yi - y_hat;
-            Eigen::Vector3d J;
+            
             J(0,0) = -xi * xi * y_hat;                      // derror/da
             J(1,0) = -xi * y_hat;                           // derror/db
             J(2,0) = -y_hat;                                // derror/dc
 
-            
             H += J * J.transpose();
+            diag_H += H.diagonal().asDiagonal();
             b += -error * J;
-
             cost += error * error;
         }
 
-        // Sovle Hx = b
-        Eigen::Vector3d dx = H.ldlt().solve(b);
+        // Sovle (H+lambda*diag(H))x = b
+        Eigen::Vector3d dx = (H + lambda * diag_H).ldlt().solve(b);
         if (isnan(dx[0])) {
             std::cout << "Result is NaN!\n";
             break;
@@ -58,10 +59,28 @@ int main(int argc, char** argv) {
             break;
         }
 
-        ae += dx(0,0);
-        be += dx(1,0);
-        ce += dx(2,0);
+        // Trust region
+        double new_cost = 0;
+        double a_temp = ae + dx(0,0);
+        double b_temp = be + dx(1,0);
+        double c_temp = ce + dx(2,0);
 
+        for (size_t i = 0; i < N; ++i) {
+            double xi = x_data[i], yi = y_data[i];          // The i-th data
+            double y_hat = std::exp(a_temp*xi*xi + b_temp*xi + c_temp); // Estimated y
+            double error = yi - y_hat;
+            new_cost += error * error;
+        }
+        
+        if (new_cost < cost) {
+            lambda /= 10;
+            ae += dx(0,0);
+            be += dx(1,0);
+            ce += dx(2,0);
+        } else {
+            lambda *= 10;
+        }
+        std::cout << "lambda: " << lambda << std::endl;
         lastCost = cost;
         std::cout << iter << ". total cost: " << cost << ", \t\t\tupdate: " << dx.transpose() << "\t\t\testimated params: " << ae << "," << be << "," << ce << std::endl;
     }
